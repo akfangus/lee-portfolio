6073d160855786fd7d7d991620b12c8391a20642
akfangus <akfanakfan@naver.com>
2025-11-28 18:34:14 +0900

    feat: 폰트 적용

--- src/features/main.tsx
+++ src/features/main.tsx
@@ -2,10 +2,16 @@ import { HieutIcon, IeungIcon, SiotIcon } from "@/components/ui/svg";
 
 export default function Main() {
   return (
-    <div className="flex items-center justify-center">
-      <IeungIcon className="w-10 h-10" />
-      <SiotIcon className="w-10 h-10" />
-      <HieutIcon className="w-10 h-10" />
+    <div className="flex flex-col items-center justify-center">
+      <div>
+        <IeungIcon className="w-10 h-10" />
+        <SiotIcon className="w-10 h-10" />
+        <HieutIcon className="w-10 h-10" />
+      </div>
+      <div>
+        <p>안녕하세요 프론트엔드 개발자 </p>
+        <p>이신행입니다</p>
+      </div>
     </div>
   );
 }
