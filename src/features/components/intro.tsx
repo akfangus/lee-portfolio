0963de06731a2e769e6f06895649e715540b6314
akfangus <akfanakfan@naver.com>
2025-11-30 13:07:16 +0900

    feat: 애니매이션 커스텀 속성 추가 및 적용

--- src/features/components/intro.tsx
+++ src/features/components/intro.tsx
@@ -2,15 +2,18 @@ import { HieutIcon, IeungIcon, SiotIcon } from "@/components/ui/svg";
 
 export function Intro() {
   return (
-    <section id="intro" className="w-full h-[90vh]">
+    <section
+      id="intro"
+      className=" flex flex-col items-center justify-center w-full gap-4 h-[90vh]"
+    >
       <div className="flex items-center justify-center">
-        <IeungIcon className="w-10 h-10" />
-        <SiotIcon className="w-10 h-10" />
-        <HieutIcon className="w-10 h-10" />
+        <IeungIcon className="w-32 h-32 intro-icon" />
+        <SiotIcon className="w-32 h-32 intro-icon" />
+        <HieutIcon className="w-32 h-32 intro-icon" />
       </div>
-      <div>
-        <p>안녕하세요 프론트엔드 개발자 </p>
-        <p>이신행입니다</p>
+      <div className="flex flex-col items-center justify-center ">
+        <p className="text-2xl ">안녕하세요 프론트엔드 개발자 </p>
+        <p className="text-2xl ">이신행입니다</p>
       </div>
     </section>
   );
